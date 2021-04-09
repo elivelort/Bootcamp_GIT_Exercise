e98961089d2ba17b1340b4ee7344135e8574ea5e
Eli <eli.velort@gmail.com>
2021-04-08 19:01:05 -0500

    create add and substract methods

---
+++ src/calculator/BasicCalculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class BasicCalculator {
+
+	public static int sumar(int a, int b) {
+		return a+b;		
+	}
+	
+	public static int restar(int a, int b) {
+		return a-b;		
+	}
+	
+}
